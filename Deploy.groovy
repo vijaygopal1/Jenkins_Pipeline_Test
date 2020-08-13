a57ccee99fc6d7ee68e900dc5dd08bde7e30f68a
Vijayendra G <gvijayen@ford.com>
2020-08-13 19:24:43 +0530

    New File added

--- Deploy.groovy
+++ Deploy.groovy
@@ -1,7 +1,37 @@
 pipeline {
     agent any
+  
+    
 
-    options {
-        ansiColor('xterm')
-    }
-}
+    stages {
+
+        stage('Pre-check') {
+            steps {
+                echo "Prep for Deployment"
+            }
+        }
+
+            stage('Display date & Current time') {
+                steps {
+                    script{
+                        sh label: '', script: 'date +"%m-%d-%Y"'
+                    }
+               
+                }
+                }
+                
+            
+
+                stage('Post-check') {
+                    
+                    steps {
+                        echo "Deployment is Failed Due no Twx Installed"
+                    }
+                }
+            
+                    
+                    
+                
+            
+        }
+  }  
